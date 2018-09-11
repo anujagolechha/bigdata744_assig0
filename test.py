7b6f800848f7031ffd303a95932d5f34306b3cc1
anujagolechha <33588261+anujagolechha@users.noreply.github.com>
2018-09-10 20:49:01 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import multiprocessing as mp
+import os
+
+
+mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # e.g. 4015976448
+mem_gib = mem_bytes/(1024.**3)  # e.g. 3.74
+
+print "No. of cores : ", mp.cpu_count()
+print "Memory (GB): ", mem_gib
