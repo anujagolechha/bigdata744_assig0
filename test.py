19e8be7f00b384de378fce612f5aaf1706c959f7
golechha <golechha@node-0.golechha-assig0.uwmadison744-f18-pg0.wisc.cloudlab.us>
2018-09-10 21:03:56 -0500

    minor changes

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import multiprocessing as mp
 import os
 
-
 mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # e.g. 4015976448
 mem_gib = mem_bytes/(1024.**3)  # e.g. 3.74
 
 print "No. of cores : ", mp.cpu_count()
-print "Memory (GB): ", mem_gib
+print "Memory (GiB) : ", mem_gib
+print "Page size : ", os.sysconf('SC_PAGE_SIZE')
+
